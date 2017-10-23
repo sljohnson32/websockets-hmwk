cbc1dc2fb5926336d8468d4ab71ea7fece33aa95
sljohnson32 <sljohnson32@gmail.com>
2017-10-23 17:43:27 -0600

    Add user name functionality

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,23 @@
 const socket = io();
 
+let name = '';
+
+$('#user-name').keyup((e) => {
+  $('button').attr('disabled', e.target.value == "" ? true : false);
+})
+
 $('form').submit(() => {
-  socket.emit('chat message', $('#m').val());
+  if (!name) {
+    name = $('#user-name').val();
+    $('#user-name').attr('value', name).attr('disabled', true);
+  }
+  socket.emit('chat message', { user: name, text: $('#m').val() });
   $('#m').val('');
   return false;
 });
 
 socket.on('chat message', (message) => {
-  $('#messages').append($('<li>').text(message));
+  $('#messages').append($('<li>').html(`<h4>${message.user}: </h4><p>${message.text}</p>`));
 });
 
 socket.on('disconnected user', (message) => {
